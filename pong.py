cf2f439de02d124786964cc974f603dd2c19a031
Mike Laird <mike.laird@gmail.com>
2017-05-13 08:30:44 -0400

    add ball and temporary basic movement

--- pong.py
+++ pong.py
@@ -1,11 +1,18 @@
+#!C:\Python27\python.exe
+
 import pygame, sys
 from pygame.locals import *
+import random
+import math
+import euclid  # from https://pypi.python.org/pypi/euclid
 
 pygame.init()
 
 background_image_filename = 'background.jpg'
 SCREEN_SIZE = (800, 480)
 paddle_size = (7, 30)
+ball_size = (7, 7)
+ball_pos = (397, 237)
 
 r_paddle_x = 700
 r_paddle_y = 240-(paddle_size[1]/2)
@@ -17,6 +24,9 @@ l_move = 0
 # define variables for fps
 fps_limit = 600
 
+# define the initial velocity
+initial_velocity = 80
+
 # get the clock object
 clock = pygame.time.Clock()
 
@@ -26,6 +36,37 @@ pygame.display.set_caption("Pong!")
 
 background = pygame.image.load(background_image_filename).convert()
 
+class Ball:
+    def __init__(self, position, size, color = (0, 0, 0), velocity = euclid.Vector2(0,0), width = 0):
+        # use a position vector instead of x and y coordinates
+        self.position = position
+        self.size = size 
+        self.color = color 
+        self.width = width
+        self.velocity = velocity
+        
+    def draw(self):
+        rx, ry = int(self.position.x), int(self.position.y)
+        pygame.draw.rect(screen, self.color, Rect((rx, ry), self.size))
+        
+    def move(self):
+        self.position += self.velocity * dtime_s    
+     
+#######################################################
+# this is just temporary
+def get_random_velocity():
+    new_angle = random.uniform(math.pi*0.25, math.pi*0.75)
+    #new_angle = math.pi*0.25
+    new_x = math.sin(new_angle)
+    new_y = math.cos(new_angle)
+    new_vector = euclid.Vector2(new_x, new_y)
+    new_vector.normalize()
+    new_vector *= initial_velocity   # pixels per second
+    return new_vector
+########################################################     
+     
+ball = Ball(euclid.Vector2(ball_pos[0], ball_pos[1]), ball_size, (255, 255, 255), get_random_velocity())
+     
 while True:
     # limit the frame rate and capture the time in 
     # milliseconds since the last tick (dtime_ms)
@@ -57,6 +98,9 @@ while True:
             
     # draw the background
     screen.blit(background, (0,0))
+    
+    # lock the screen
+    screen.lock() 
         
     # adjust y-coordinate to account for paddle movement
     r_paddle_y += r_move
@@ -79,7 +123,13 @@ while True:
     # draw the paddle rectangles
     pygame.draw.rect(screen, (255,255,255), Rect(r_paddle_pos, paddle_size))
     pygame.draw.rect(screen, (255,255,255), Rect(l_paddle_pos, paddle_size))
-        
+    
+    ball.move()
+    ball.draw()
+    
+    # unlock the screen
+    screen.unlock()
+    
     # call update to redraw the screen    
     pygame.display.update()
     
